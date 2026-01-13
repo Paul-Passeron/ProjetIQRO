618fdd071df17827206f2c0baea839b753e73039
Paul-Passeron <paul.passeron2@gmail.com>
2026-01-13 11:45:19 +0100

    Add MaxOrSat class with input validation

--- code/main.py
+++ code/main.py
@@ -1,1 +1,13 @@
-print("Hello, World !")
+import numpy as np
+import numpy.typing as npt
+
+
+class MaxOrSat:
+    def __init__(self, A: npt.NDArray[np.bool_], b: npt.NDArray[np.bool_]):
+        m = A.shape[0]
+        n = A.shape[1]
+        assert b.shape[0] == m
+        for x in A:
+            assert x.shape[0] == n
+        self.A = A
+        self.b = b
