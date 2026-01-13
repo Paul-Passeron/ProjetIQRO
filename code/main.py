ef2fcf89c6dc56972344f757a6839878be4fb151
Rayan Merkhi <rayanmerkhi@yahoo.fr>
2026-01-13 16:13:31 +0100

    ajout de l'algorithme de grover

--- code/main.py
+++ code/main.py
@@ -1,5 +1,16 @@
+from qiskit import QuantumCircuit
+from qiskit.circuit.library import HGate, MCXGate
 import numpy as np
 import numpy.typing as npt
+
+ 
+mcx_gate = MCXGate(3)
+hadamard_gate = HGate()
+ 
+qc = QuantumCircuit(4)
+qc.append(hadamard_gate, [0])
+qc.append(mcx_gate, [0, 1, 2, 3])
+qc.draw("mpl")
 
 
 class MaxXorSat:
